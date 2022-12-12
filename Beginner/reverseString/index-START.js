e3e381f1060e84993c3e5bc61f2fa51c1358ada7
Victor Patru <patruvictor.mihai@gmail.com>
2022-12-12 22:21:02 +0200

    add reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,26 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
 function reverseString(text) {
-    // Code goes here
+  // **Method Chaining
+  //   return [...text].reverse().join("");
+  // **Recursive
+  //   if (text === "") {
+  //     return "";
+  //   } else {
+  //     return reverseString(text.slice(1)) + text[0];
+  //   }
+  // ** For loop
+  let result = "";
+
+  for (let char of text) {
+    result = char + result;
+    console.log(result, char);
+  }
+
+  return result;
+  // ** Reduce
+  //   return [...text].reduce((acc, val) => val + acc, "");
 }
 
-
-
-module.exports = reverseString
+module.exports = reverseString;
